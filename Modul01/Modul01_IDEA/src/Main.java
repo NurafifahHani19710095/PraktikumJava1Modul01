458c51e8478a3ddfef49163946d1eec72215f1aa
Hanni Suka <hannisuka99@hotmail.com>
2021-06-16 11:34:00 +0700

    Operasi String

--- Modul01/Modul01_IDEA/src/Main.java
+++ Modul01/Modul01_IDEA/src/Main.java
@@ -15,5 +15,21 @@ public class Main {
         char [] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("fah"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(4,6));
+        System.out.println(namaLengkap.replace("fifa","ni"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for(String nama : namaArray) {
+            System.out.println(nama);
+        }
     }
 }
